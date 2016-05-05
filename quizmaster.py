342bdd4fac721d1b08e64b64117298606a2d51c2
Robert Webb <bob.wbb@gmail.com>
2016-05-05 21:00:22 +0100

    new questions and infinite length gameplay hurrah

--- quizmaster.py
+++ quizmaster.py
@@ -1,29 +1,49 @@
+import operator
+import random
+
 import networkzero as nw0
+
+MAX_QUESTNUM = 20
 
 t = 'server_team_4'
 a = nw0.advertise(t)
 
+def multiply_the_Things(y,x):
+    a = 0
+    for i in range(x):
+        a += y
+    return a
 
-question = "What is 2 + 2?"
-expected_answer = "4"
+operations = {
+    '+' : lambda x,y:x+y,
+    '-' : operator.sub,
+    '*' : multiply_the_Things
+}
+
+def questionator3000():
+    n1 = random.randint(0, MAX_QUESTNUM)
+    n2 = random.randint(0, MAX_QUESTNUM)
+    o, op = random.choice(list(operations.items()))
+    return "{} {} {}".format(n1,o,n2), str(op(n1,n2))
 
 
-greeting = nw0.wait_for_message_from(a)
-print("-> {}".format(greeting))
+while True:
+    greeting = nw0.wait_for_message_from(a)
+    print("-> {}".format(greeting))
+
+    question, expected_answer = questionator3000()
+
+    nw0.send_reply_to(a, question)
+    print("<- {}".format(question))
+
+    answer = nw0.wait_for_message_from(a)
+    print("-> {}".format(answer))
 
 
-nw0.send_reply_to(a, question)
-print("<- {}".format(question))
+    if answer == expected_answer:
+        outcome = "correct!"
+    else:
+        outcome = "incorrect!"
 
-
-answer = nw0.wait_for_message_from(a)
-print("-> {}".format(answer))
-
-
-if answer == expected_answer:
-    outcome = "correct!"
-else:
-    outcome = "incorrect!"
-
-nw0.send_reply_to(a, outcome)
-print("<- {}".format(outcome))
+    nw0.send_reply_to(a, outcome)
+    print("<- {}, the correct answer was {}".format(outcome, expected_answer))
