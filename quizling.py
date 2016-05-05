762aa07f92ffe5bafbdd8773fc36e179ee2e840f
TomV <tom@viner.tv>
2016-05-05 20:59:44 +0100

    loop the player

--- quizling.py
+++ quizling.py
@@ -1,16 +1,24 @@
+import ast
+import itertools
+
 import networkzero as nw0
 
 t = 'server_team_4'
-
 a = nw0.discover(t)
 
-question = nw0.send_message_to(a, "-> I'm ready for the quiz!")
+def get_ans(question):
+    return str(ast.literal_eval(question))
+    return input("What's the answer? ")
 
-print('<- {}'.format(question))
+for round in itertools.count(1):
+    print('ROUND {}'.format(round))
+    question = nw0.send_message_to(a, "-> I'm ready!")
 
-ans = input("What's the answer? ")
-print("-> {}".format(ans))
+    print('<- {}'.format(question))
+    ans = get_ans(question)
+    print("-> {}".format(ans))
 
-n = nw0.send_message_to(a, ans)
+    n = nw0.send_message_to(a, ans)
 
-print('<- {}'.format(n))
+    print('<- {}'.format(n))
+    print()
